c9cd9678d3feb5d2a5e608899ee78b3ddbbb7e05
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-03-31 20:37:24 +0500

    add functionality to the submit button to display the overlay form for book submission and cancel button takes the user back to the homepage

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ function addBookToLibrary(name,author,totalPages,yearOfRelease,read)
     book=new makeBook(name,author,totalPages,yearOfRelease,read);
     library.push(book);
 }
+let submitButton = document.querySelector(".addBook");
+let modalOverlay=document.querySelector(".modal-overlay");
+let inputFields=document.querySelectorAll("input");
+let cancelSubmission = document.querySelector(".cancelButton");
+cancelSubmission.addEventListener("click",()=>
+{
+    modalOverlay.classList.add("hidden");
+    inputFields.forEach(input => input.value="");
+})
+submitButton.addEventListener("click",()=>
+{
+    modalOverlay.classList.remove("hidden");
+})
+console.log("Script Is Working");
