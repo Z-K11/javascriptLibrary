4f383099a470eb5c47176cb39fa9048a0f9eff6d
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-03-31 21:22:47 +0500

    add event listner to submit the inputs as arguements to a function call that calls the constructor to create a new book object

--- script.js
+++ script.js
@@ -18,13 +18,26 @@ let submitButton = document.querySelector(".addBook");
 let modalOverlay=document.querySelector(".modal-overlay");
 let inputFields=document.querySelectorAll("input");
 let cancelSubmission = document.querySelector(".cancelButton");
+let bookCreator=document.querySelector(".createBook");
+let bookName=document.querySelector("#name");
+let authorName=document.querySelector("#author");
+let bookPages=document.querySelector("#pages");
+let releaseYear = document.querySelector("#bookRelease");
+
 cancelSubmission.addEventListener("click",()=>
 {
     modalOverlay.classList.add("hidden");
     inputFields.forEach(input => input.value="");
-})
+});
 submitButton.addEventListener("click",()=>
 {
     modalOverlay.classList.remove("hidden");
-})
+});
 console.log("Script Is Working");
+bookCreator.addEventListener("click",()=>
+{
+    addBookToLibrary(bookName.value,authorName.value,bookPages.value,releaseYear.value,document.querySelector('input[type="radio"]:checked'));
+    modalOverlay.classList.add("hidden");
+    inputFields.forEach(input => input.value="");
+    console.log(library);
+});
