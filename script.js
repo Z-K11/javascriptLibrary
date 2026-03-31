a0dc6af0e11fee2a0eec534d22ada59111a5cfa9
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-03-31 15:59:26 +0500

    write the necessary html for the modal-ui-overlay

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let library = [];
-function makeBook (name,author,totalPages,yearOfRelease,uid)
+function makeBook (name,author,totalPages,yearOfRelease,read)
 {
     this.name=name;
     this.author=author;
     this.totalPages=totalPages;
     this.yearOfRelease=yearOfRelease;
-    this.uid=uid;
+    this.read=read
+    this.uid=crypto.randomUUID;
     console.log(`${this.name} stored succesfully`);
 }
-function addBookToLibrary(name,author,totalPages,yearOfRelease,uid)
+function addBookToLibrary(name,author,totalPages,yearOfRelease,read)
 {
-    book=new makeBook(name,author,totalPages,yearOfRelease,uid);
+    book=new makeBook(name,author,totalPages,yearOfRelease,read);
     library.push(book);
 }
