0cc8bca763a7a29161c717aef361fe5c113a6137
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-04-01 09:14:26 +0500

    Implement functionality that when a book is submitted to the website it is also displayed in a table

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function makeBook (name,author,totalPages,yearOfRelease,read)
     this.totalPages=totalPages;
     this.yearOfRelease=yearOfRelease;
     this.read=read
-    this.uid=crypto.randomUUID;
+    this.uid=crypto.randomUUID();
     console.log(`${this.name} stored succesfully`);
 }
 function addBookToLibrary(name,author,totalPages,yearOfRelease,read)
@@ -23,6 +23,7 @@ let bookName=document.querySelector("#name");
 let authorName=document.querySelector("#author");
 let bookPages=document.querySelector("#pages");
 let releaseYear = document.querySelector("#bookRelease");
+let table=document.querySelector("tbody");
 
 cancelSubmission.addEventListener("click",()=>
 {
@@ -36,13 +37,24 @@ submitButton.addEventListener("click",()=>
 console.log("Script Is Working");
 bookCreator.addEventListener("click",()=>
 {
-    if(!bookName.value|!authorName.value|isNaN(bookPages.value)|isNaN(releaseYear.value))
+    if(!bookName.value||!authorName.value||isNaN(bookPages.value)||isNaN(releaseYear.value))
     {
         alert("Invalid or No Input");
         return;
     }
-    addBookToLibrary(bookName.value,authorName.value,bookPages.value,releaseYear.value,document.querySelector('input[type="radio"]:checked'));
+    addBookToLibrary(bookName.value,authorName.value,bookPages.value,releaseYear.value,document.querySelector('input[type="radio"]:checked').value);
     modalOverlay.classList.add("hidden");
     inputFields.forEach(input => input.value="");
     console.log(library);
+    let newRow=table.insertRow(-1);
+    let cell1 = newRow.insertCell(0);    
+    let cell2 = newRow.insertCell(1);    
+    let cell3 = newRow.insertCell(2);    
+    let cell4 = newRow.insertCell(3);    
+    let cell5 = newRow.insertCell(4);
+    cell1.textContent=library[library.length-1].name;
+    cell2.textContent=library[library.length-1].author;
+    cell3.textContent=library[library.length-1].totalPages;
+    cell4.textContent=library[library.length-1].yearOfRelease;
+    cell5.textContent=library[library.length-1].read;
 });
